e43f8781ac980780bf97b9273d47aee18c28744d
Vasanth Developer <vsnthdev@gmail.com>
2022-01-30 19:46:22 +0530

    🔨 api: server now restarts during development

--- api/src/logger.ts
+++ api/src/logger.ts
@@ -5,7 +5,14 @@
 
 import itivrutaha from 'itivrutaha';
 import { Logger } from 'itivrutaha/dist/class';
+import chalk from 'chalk';
 
-export default async (): Promise<Logger> => await itivrutaha.createNewLogger({
-    appName: '@alpa/api'
-})
+export default async (): Promise<Logger> => {
+    const log = await itivrutaha.createNewLogger({
+        appName: '@alpa/api'
+    })
+
+    log.note(`Running in ${chalk.whiteBright.bold(process.env.NODE_ENV ? process.env.NODE_ENV : 'development')} mode`)
+
+    return log
+}
