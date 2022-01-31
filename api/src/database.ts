af77294fb161332ba5d49b1b7d3199809790f4a0
Vasanth Developer <vsnthdev@gmail.com>
2022-01-31 13:13:24 +0530

    ✨ api: database connection failure is now handled

--- api/src/database.ts
+++ api/src/database.ts
@@ -8,18 +8,31 @@ import { Logger } from 'itivrutaha/dist/class'
 import { AlpaAPIConfig } from './config/interface.js'
 import chalk from 'chalk';
 
-export default (log: Logger, { database }: AlpaAPIConfig) => {
+interface ConnectionsList {
+    codes: Redis.Redis
+}
+
+export default async (log: Logger, { database }: AlpaAPIConfig) => {
     const base = {
         host: database.host,
         port: database.port,
         password: database.password
     }
 
-    const conn = {
-        codes: new Redis({ ...base, ...{ db: database.channels.codes } } as any)
+    const conn: ConnectionsList = {
+        codes: null
+    }
+
+    const failedConnecting = () => log.error('Failed connecting to the database.', 2)
+
+    conn.codes = new Redis({ ...base, ...{ db: database.channels.codes } } as any)
+    
+    for (const key in conn) {
+        const db = conn[key] as Redis.Redis
+        db.on('error', failedConnecting)
+        await db.info()
     }
 
     log.success(`Connected to Redis database at ${chalk.gray.underline(`redis://${base.host}:${base.port}`)}`)
-
     return conn
 }
