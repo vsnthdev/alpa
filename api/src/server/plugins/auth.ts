b775846d0b86b49ce7761a467bb3f4a38ea3014d
Vasanth Developer <vsnthdev@gmail.com>
2022-02-12 00:21:49 +0530

    ✨ api: auth plugin now checks for blacklisted tokens

--- api/src/server/plugins/auth.ts
+++ api/src/server/plugins/auth.ts
@@ -7,8 +7,17 @@ import { FastifyReply, FastifyRequest } from 'fastify'
 import fp from 'fastify-plugin'
 import boom from 'boom';
 
+import { db } from '../../database.js';
+
 const func: any = async (req: FastifyRequest, rep: FastifyReply) => {
     try {
+        // check if that token exists in our black list
+        // if yes, then we simply throw an error, so it gets
+        // caught by the try/catch and throws 401 Unauthorized
+        const token = req.headers.authorization.slice(7)
+        if (await db.tokens.exists(token)) throw new Error('Unauthorized')
+
+        // if the token isn't in our black list then we check for validity
         await req.jwtVerify()
     } catch (err) {
         throw boom.unauthorized()
