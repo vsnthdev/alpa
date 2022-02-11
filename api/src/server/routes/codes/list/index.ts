19ea32dc895990bb74e27cbb65cda042caf2ec0a
Vasanth Developer <vsnthdev@gmail.com>
2022-02-11 15:52:01 +0530

    ✨ api: GET /api/codes now lists full code objects

--- api/src/server/routes/codes/list/index.ts
+++ api/src/server/routes/codes/list/index.ts
@@ -4,9 +4,17 @@ import { AlpaAPIConfig } from '../../../../config/interface.js';
 import { ConnectionsList } from '../../../../database.js';
 
 const getHandler = (config: AlpaAPIConfig, db: ConnectionsList) => async (req: FastifyRequest, rep: FastifyReply): Promise<any> => {
-    const codes = await db.codes.keys('*')
+    const cursor = req.query['cursor'] || 0
+    const [now, keys] = await db.codes.scan(cursor)
 
-    return rep.status(200).send({ codes })
+    const codes = []
+
+    for (const key of keys) {
+        const str = await db.codes.get(key)
+        codes.push({ ...{ code: key }, ...JSON.parse(str) })
+    }
+
+    return rep.status(200).send({ cursor: now, codes })
 }
 
 export default {
