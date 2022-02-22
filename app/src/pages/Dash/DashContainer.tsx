c10c90a87e35ba1ed9b38bee608ad6d9cf39e7f1
Vasanth Developer <vsnthdev@gmail.com>
2022-02-22 17:58:12 +0530

    ✨ app: DashContent now has smooth reveal animation

--- app/src/pages/Dash/DashContainer.tsx
+++ app/src/pages/Dash/DashContainer.tsx
@@ -34,28 +34,39 @@ export const Dash = (): ReactElement => {
     // the codes of using the token
     useEffect(() => {
         progress.start()
-        axios({
-            method: 'GET',
-            url: `${apiHost}/api/codes`,
-            headers: {
-                Authorization: `Bearer ${apiToken}`
-            }
-        }).then(({status, data}) => {
-            if (status == 200) {
-                // update our store with the latest
-                // user details
-                const { username, email } = parseJWTPayload(apiToken)
-                dispatch(login({
-                    apiHost,
-                    apiToken,
-                    username,
-                    email,
-                    isLoggedIn: true,
-                }))
-
-                dispatch(insert(data.codes))
-                setLoading(false)
-            } else {
+        setTimeout(() => {
+            axios({
+                method: 'GET',
+                url: `${apiHost}/api/codes`,
+                headers: {
+                    Authorization: `Bearer ${apiToken}`
+                }
+            }).then(({status, data}) => {
+                if (status == 200) {
+                    // update our store with the latest
+                    // user details
+                    const { username, email } = parseJWTPayload(apiToken)
+                    dispatch(login({
+                        apiHost,
+                        apiToken,
+                        username,
+                        email,
+                        isLoggedIn: true,
+                    }))
+    
+                    dispatch(insert(data.codes))
+                    setLoading(false)
+                } else {
+                    logout({
+                        auth: {
+                            apiHost,
+                            apiToken
+                        },
+                        dispatch,
+                        navigate
+                    })
+                }
+            }).catch(err => {
                 logout({
                     auth: {
                         apiHost,
@@ -64,23 +75,14 @@ export const Dash = (): ReactElement => {
                     dispatch,
                     navigate
                 })
-            }
-        }).catch(err => {
-            logout({
-                auth: {
-                    apiHost,
-                    apiToken
-                },
-                dispatch,
-                navigate
+            }).finally(() => {
+                progress.done()
             })
-        }).finally(() => {
-            progress.done()
-        })
+        }, 3000)
     }, [])
 
     return <main>
         <DashHero loading={loading} modalState={modalState} quickText={quickText} setQuickText={setQuickText}></DashHero>
-        <DashContent modalState={modalState} quickText={quickText}></DashContent>
+        <DashContent modalState={modalState} quickText={quickText} loading={loading}></DashContent>
     </main>
 }
--- app/src/pages/Dash/DashContent.tsx
+++ app/src/pages/Dash/DashContent.tsx
@@ -11,7 +11,7 @@ import { AppState } from "../../store";
 import { CodeModal } from '../../components/CodeModal/CodeModal';
 import { CodeModalStateReturns } from '../../components/CodeModal/functions';
 
-export const DashContent = ({ modalState, quickText }: { modalState: CodeModalStateReturns, quickText: string }): ReactElement => {
+export const DashContent = ({ modalState, quickText, loading }: { modalState: CodeModalStateReturns, quickText: string, loading: boolean }): ReactElement => {
     // pull codes from the store
     const codes = useSelector((state: AppState) => state.codes)
 
@@ -20,7 +20,7 @@ export const DashContent = ({ modalState, quickText }: { modalState: CodeModalSt
         progress.done()
     }, [])
 
-    return <div className="py-24 container mx-auto flex flex-col items-center">
+    return <div className={`py-24 container mx-auto flex flex-col items-center transition-opacity ${loading ? 'opacity-0' : 'opacity-100'}`}>
         <h1 className="text-3xl font-semibold mb-16 md:text-4xl">Recently created</h1>
         <div className="flex flex-col w-full max-w-3xl px-8 items-center space-y-8">
             { codes.filter(code => [code.code, code.links[0].url, code.tags].join(' ').includes(quickText)).map(code => <CodeCard key={code.code} code={code} modalState={modalState}></CodeCard> ) }
