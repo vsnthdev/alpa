2123fd4dec6f3a98d6d24a02e393f72ab7da5a7c
Vasanth Developer <vsnthdev@gmail.com>
2022-05-16 01:41:12 +0530

    ⚡ api: now does 301 redirects

--- api/src/server/routes/code/index.ts
+++ api/src/server/routes/code/index.ts
@@ -14,16 +14,25 @@ export interface ParamsImpl {
 }
 
 const handler = async (req: FastifyRequest, rep: FastifyReply) => {
+    // read the URL parameters
     const params = req.params as ParamsImpl
 
+    // determine the code, or use _root if no code is provided
     const links = (await db.codes.json.get(params.code || '_root', {
         path: ['links'],
     })) as CodeLink[]
 
+    // handle when links don't exist
     if (!links) throw boom.notFound()
 
+    // apply caching headers
+    rep.header('Cache-Control', 'max-age=60')
+
+    // only single link redirection has been implemented till now
+    // for multiple, we either send a JavaScript response
+    // or a full HTML page rendered on the server
     if (links.length == 1) {
-        return rep.redirect(307, links[0].url)
+        return rep.redirect(301, links[0].url)
     } else {
         throw boom.notImplemented()
     }
